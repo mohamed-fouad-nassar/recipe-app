1ebbcdec09ba49fc4db27afbe49d25c0f47858bf
Mohamed-Fouad-Nassar <mohammednassar740@gmail.com>
2026-03-28 22:47:09 +0200

    feat: finish profile my recipes

--- frontend/src/app/pages/profile/my-recipes/my-recipes.ts
+++ frontend/src/app/pages/profile/my-recipes/my-recipes.ts
@@ -1,14 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { RecipeStore } from '../../../features/recipes/recipe.store';
 import { ProfileRecipeCard } from '../../../features/recipes/profile-recipe-card/profile-recipe-card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile-my-recipes',
   templateUrl: './my-recipes.html',
-  imports: [ProfileRecipeCard],
+  imports: [ProfileRecipeCard, RouterLink],
 })
-export class ProfileMyRecipes {
+export class ProfileMyRecipes implements AfterViewInit {
   store = inject(RecipeStore);
+
+  handleDelete(id: string) {
+    this.store.deleteRecipe(id);
+  }
+
+  ngAfterViewInit() {
+    initFlowbite();
+  }
 
   constructor() {
     this.store.loadMyRecipes();
